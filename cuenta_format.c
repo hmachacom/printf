0ffda850dd535ee4da9315e23ee92c3c08a91d23
jhonholberton <3425@holbertonschool.com>
2021-11-08 10:14:08 -0500

    funcion para evaluar si existe el simbolo % en la cadena

---
+++ cuenta_format.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cuenta_format - incrementa cada vez que encuentra un %
+ * @format: cadena principal
+ * Return: retorna la cantidad de % que se encuentra
+ */
+int cuenta_format(const char *format)
+{
+	int lon, i, cuentafor = 0;
+
+	lon = strlen(format) + 1;
+	for (i = 0; i < lon; i++)
+	{
+		if (format[i] == '%' && format[i + 1] == '%')
+		{
+			i++;
+			cuentafor++;
+		}
+		else if (format[i] == '%' && (format[i + 1] == 's' || format[i + 1] == 'c'))
+		{
+			cuentafor += 2;
+		}
+	}
+	return (cuentafor);
+}
